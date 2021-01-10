f0d1a1f4ee64ee1a3be03052391cf2d61a7d9016
Elex <developer@elex-project.com>
2021-01-10 10:56:27 +0900

    add maven local repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,6 +103,10 @@ publishing {
 
     repositories {
         maven {
+            name = "mavenLocal"
+            url = uri("file://media/elex/UltraFit/maven-repo")
+        }
+        maven {
             name = "mavenGithub"
             url = uri("https://maven.pkg.github.com/elex-project/xelhua")
             credentials {
